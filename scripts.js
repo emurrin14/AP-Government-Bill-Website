c0c10a0caeb3bca53b53a1b3c9bc2f1f99eefac2
emurrin14 <easye879@gmail.com>
2025-10-16 20:25:20 +0000

    new

--- scripts.js
+++ scripts.js
@@ -1,3 +1,47 @@
+window.addEventListener('load', () => {
+  const revealElements = document.querySelectorAll('.revealImg');
+
+  revealElements.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add('revealedImg');
+    }, i * 300);
+  });
+});
+
+window.addEventListener('load', () => {
+  const revealElements = document.querySelectorAll('.revealImg2');
+
+  revealElements.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add('revealedImg2');
+    }, i * 150);
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const revealElements = document.querySelectorAll('.reveal');
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('revealed');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  revealElements.forEach(element => observer.observe(element));
+});
+
+  
+
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.getElementById("main-header");
   const triggerPoint = 300; // Scroll Y (px) where header appears
@@ -8,5 +52,15 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       header.classList.remove("show");
     }
+  });
+});
+
+document.getElementById("scrollBtn").addEventListener("click", () => {
+  const image = document.getElementById("landingImgGrid");
+  const offset = image.getBoundingClientRect().bottom;
+  window.scrollBy({
+    top: offset,
+    left: 0,
+    behavior: "smooth"
   });
 });
